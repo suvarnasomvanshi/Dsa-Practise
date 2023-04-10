4e3f34fa759b81acdfae09f853043211d1739262
suvarnasomvanshi <119663386+suvarnasomvanshi@users.noreply.github.com>
2023-04-10 11:24:46 +0530

    Update codeZinger logic .js

--- codeZinger logic .js
+++ codeZinger logic .js
@@ -162,6 +162,27 @@ for(let i=0;i<string.length;i++){
 
 console.log(obj);
 
+
+
+/* kadane algorithm-------------  */
+let nums = [-2,1,-3,4,-1,2,1,-5,4,10]
+function maximumSubarry(nums){
+    let min=0;
+    let max=0
+    for(let i=0; i<nums.length; i++){
+    //   min= Math.max(min, min+nums[i])
+        min=min+nums[i]
+        max=Math.max(min,max)
+        if(min<0){
+            min=0
+        }
+        
+    }
+return max 
+}
+console.log(maximumSubarry(nums))
+
+
 //Q1  242. Valid Anagram
  //https://leetcode.com/problems/valid-anagram/
   
